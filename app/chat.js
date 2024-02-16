9eea732542fcd865abf3fa3759d0596c07223de1
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2024-02-16 13:47:24 +0100

    :sparkles: added 3 basic pages navigation

---
+++ app/chat.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+
+import { Link } from "expo-router";
+
+export default function Page() {
+  return (
+    <View>
+      <Text>Chat</Text>
+      <Link href="/about">Map</Link>
+      <Link href="/">Swipe</Link>
+      <Link href="/chat">Chat</Link>
+    </View>
+  );
+}
