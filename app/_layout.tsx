fd76768901060ff80bead4fdd3172df43696003f
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2024-03-20 09:01:23 +0100

    ➕ added redux

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,8 @@ import { useEffect } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 SplashScreen.preventAutoHideAsync();
 export default function AppLayout() {
@@ -26,49 +28,51 @@ export default function AppLayout() {
   if (!loaded) return null;
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors.primary,
-        tabBarLabelStyle: {
-          fontFamily: "RobotoLight",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="map"
-        options={{
-          title: "Map",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="map-marked-alt" size={24} color="black" />
-          ),
+    <Provider store={store}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors.primary,
+          tabBarLabelStyle: {
+            fontFamily: "RobotoLight",
+          },
         }}
-      />
-      <Tabs.Screen
-        name="swipe"
-        options={{
-          title: "Swipe",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="swipe" size={24} color="black" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="chat"
-        options={{
-          title: "Chat",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="chatbubble" size={24} color="black" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        // Name of the route to hide.
-        name="index"
-        options={{
-          // This tab will no longer show up in the tab bar.
-          href: null,
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="map"
+          options={{
+            title: "Map",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome5 name="map-marked-alt" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="swipe"
+          options={{
+            title: "Swipe",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="swipe" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="chat"
+          options={{
+            title: "Chat",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="chatbubble" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          // Name of the route to hide.
+          name="index"
+          options={{
+            // This tab will no longer show up in the tab bar.
+            href: null,
+          }}
+        />
+      </Tabs>
+    </Provider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import * as SQLite from "expo-sqlite";
 import { Link } from "expo-router";
 import * as FileSystem from "expo-file-system";
 import { Asset } from "expo-asset";
+import CounterComponent from "@/features/counter/CounterComponent";
 
 // Ajoutez un flag de débogage
 const DEBUG_MODE = true; // Mettez à false pour la production
@@ -13,40 +14,41 @@ export default function Page() {
     prepareDatabase();
   }, []);
 
-  async function prepareDatabase() {
-    const databaseFilename = "db.db";
-    const internalDbName = `${FileSystem.documentDirectory}SQLite/${databaseFilename}`;
-
-    // En mode de débogage, toujours recopier la base de données
-    if (DEBUG_MODE) {
-      console.log(
-        "Mode de débogage actif : Recopie systématique de la base de données."
-      );
-      const asset = Asset.fromModule(require("../assets/databases/db.db"));
-      await FileSystem.downloadAsync(asset.uri, internalDbName);
-    } else {
-      // En mode normal, vérifier si la base de données doit être copiée
-      const fileInfo = await FileSystem.getInfoAsync(internalDbName);
-      if (!fileInfo.exists) {
-        console.log("Copie de la base de données dans le stockage interne.");
-        const asset = Asset.fromModule(require("../assets/databases/db.db"));
-        await FileSystem.downloadAsync(asset.uri, internalDbName);
-      }
-    }
-
-    const db = SQLite.openDatabase(databaseFilename);
-    retrieveAllFromDatabaseTable(db);
-    listDatabaseTables(db);
-  }
-
   return (
     <View>
       <Text>Index / onboarding</Text>
       <Link href="/map">Map</Link>
       <Link href="/">Swipe</Link>
       <Link href="/chat">Chat</Link>
+      <CounterComponent />
     </View>
   );
+}
+
+async function prepareDatabase() {
+  const databaseFilename = "db.db";
+  const internalDbName = `${FileSystem.documentDirectory}SQLite/${databaseFilename}`;
+
+  // En mode de débogage, toujours recopier la base de données
+  if (DEBUG_MODE) {
+    console.log(
+      "Mode de débogage actif : Recopie systématique de la base de données."
+    );
+    const asset = Asset.fromModule(require("../assets/databases/db.db"));
+    await FileSystem.downloadAsync(asset.uri, internalDbName);
+  } else {
+    // En mode normal, vérifier si la base de données doit être copiée
+    const fileInfo = await FileSystem.getInfoAsync(internalDbName);
+    if (!fileInfo.exists) {
+      console.log("Copie de la base de données dans le stockage interne.");
+      const asset = Asset.fromModule(require("../assets/databases/db.db"));
+      await FileSystem.downloadAsync(asset.uri, internalDbName);
+    }
+  }
+
+  const db = SQLite.openDatabase(databaseFilename);
+  retrieveAllFromDatabaseTable(db);
+  listDatabaseTables(db);
 }
 
 function retrieveAllFromDatabaseTable(db: SQLite.Database) {
