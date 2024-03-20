b35d8881179610ee40178100f5b68f44b8e0970f
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2024-03-20 11:42:07 +0100

    :lipstick: adjustments on character chat screen

--- app/chat/[id].tsx
+++ app/chat/[id].tsx
@@ -1,12 +1,52 @@
-import { View, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { View, Text, StyleSheet } from "react-native";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { useSelector } from "react-redux";
+import { selectLikedCharacters } from "@/features/characters/charactersSlice";
+import { useEffect, useState } from "react";
 
 export default function ChatWithCharacterPage() {
   const { id } = useLocalSearchParams();
+  const likedCharacters = useSelector(selectLikedCharacters);
+  const [character, setCharacter] = useState<Character | null>(null);
+
+  useEffect(() => {
+    // Trouver le personnage par id
+    const char = likedCharacters.find((c) => c.id.toString() === id);
+    setCharacter(char ?? null);
+  }, [id, likedCharacters]);
+
+  if (!character) {
+    return (
+      <View style={styles.container}>
+        <Stack.Screen
+          options={{
+            title: "Introuvable",
+          }}
+        />
+        <Text>Discussion introuvable</Text>
+      </View>
+    );
+  }
 
   return (
-    <View>
-      <Text>Chat avec character {id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: `${character.name} ${character.surname ?? ""}`,
+          headerBackTitle: "Retour",
+        }}
+      />
+      {/* Afficher les détails du personnage ici */}
+      <Text>{character.name}</Text>
+      {/* Autres détails */}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- app/chat/index.tsx
+++ app/chat/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, FlatList, StyleSheet, Text } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import { selectLikedCharacters } from "../../features/characters/charactersSlice";
 import CharacterChatItem from "@/features/characters/CharacterChatItem";
