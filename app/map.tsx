536b8ec8c750ec1dd619c32d2550f8d30df8fb83
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2024-03-31 18:15:04 +0200

    :bug: fixed error when trying to load map on browser

--- app/map.tsx
+++ app/map.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View, Text } from 'react-native';
 
-export default function Map () {
+export default function Map() {
+  if (Platform.OS !== 'web') {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.map} />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
+    <View style={styles.centeredContainer}>
+      <Text>La carte n&apos;est pas encore dispo sur navigateur</Text>
     </View>
   );
 }
@@ -17,5 +25,10 @@ const styles = StyleSheet.create({
   map: {
     width: '100%',
     height: '100%',
+  },
+  centeredContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
