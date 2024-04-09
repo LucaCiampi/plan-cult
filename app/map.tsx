d1b44bf047a03beec63409198b9238c52b73af24
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2024-04-09 20:34:42 +0200

    :sparkles: added working map with cursor

--- app/map.tsx
+++ app/map.tsx
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import { Platform, StyleSheet, View, Text } from 'react-native';
 import cursorPinReference from '@/assets/images/icon.png';
 
+interface Region {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+}
+
 export default function Map() {
-  const coords = {
+  const [markers] = useState([
+    {
+      title: 'titre',
+      latlng: {
+        latitude: 45.767135,
+        longitude: 4.833658,
+      },
+    },
+  ]);
+
+  const [region, setRegion] = useState<Region>({
     latitude: 45.767135,
     longitude: 4.833658,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  // Gestionnaire de changement de région
+  const handleRegionChange = (newRegion: Region) => {
+    setRegion(newRegion);
   };
 
   if (Platform.OS !== 'web') {
     return (
       <View style={styles.container}>
-        <MapView style={styles.map} />
-        <Marker coordinate={coords} image={{ uri: cursorPinReference }} />
+        <MapView
+          style={styles.map}
+          region={region}
+          onRegionChange={handleRegionChange}
+        >
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              coordinate={marker.latlng}
+              title={marker.title}
+              image={cursorPinReference}
+            />
+          ))}
+          {/* <Overlay
+            bounds={[
+              [45.72, 4.8],
+              [45.78, 4.87],
+            ]}
+            image={cursorPinReference}
+          /> */}
+        </MapView>
       </View>
     );
   }
