41aca53e7f74618527f41888af2ebcf849fbb931
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2024-04-23 10:17:37 +0200

    :sparkles: added "hello world" example AR scene

--- app/profile/index.tsx
+++ app/profile/index.tsx
@@ -1,15 +1,66 @@
+import {
+  ViroARScene,
+  ViroARSceneNavigator,
+  ViroText,
+  ViroTrackingReason,
+  ViroTrackingStateConstants,
+} from '@viro-community/react-viro';
 import { Stack } from 'expo-router';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
-export default function Page() {
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState('Initializing AR...');
+
+  function onInitialized(state: any, reason: ViroTrackingReason) {
+    console.log('onInitialized', state, reason);
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setText('Hello World!');
+    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
+      // Handle loss of tracking
+    }
+  }
+
   return (
-    <View>
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroText
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
+      />
+    </ViroARScene>
+  );
+};
+
+const ARSceneNavigator = () => {
+  return (
+    <View style={styles.container}>
       <Stack.Screen
         options={{
           headerShown: false,
         }}
       />
-      <Text>Profile</Text>
+      <ViroARSceneNavigator
+        autofocus={true}
+        initialScene={{
+          scene: HelloWorldSceneAR,
+        }}
+        style={styles.container}
+      />
     </View>
   );
-}
+};
+
+export default ARSceneNavigator;
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
